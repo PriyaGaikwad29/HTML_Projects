afe98ff89ce64550f80a6ce882648ccedef56760
PriyaGaikwad29 <147377979+PriyaGaikwad29@users.noreply.github.com>
2023-10-19 16:44:18 +0530

    Add files via upload

---
+++ RE.js
@@ -0,0 +1,100 @@
+function a()
+{
+    document.getElementById("image1").src="./images/Motorcycles/M1.jpg";
+}
+function b()
+{
+    document.getElementById("image1").src="./images/Motorcycles/M2.webp";
+}
+function c()
+{
+    document.getElementById("image1").src="./images/Motorcycles/M3.jfif";
+}
+function d()
+{
+    document.getElementById("image1").src="./images/Motorcycles/M4.webp";
+}
+function e()
+{
+    document.getElementById("image1").src="./images/Motorcycles/M5.webp";
+}
+function f()
+{
+    document.getElementById("image1").src="./images/Motorcycles/M6.webp";
+}
+function g()
+{
+    document.getElementById("image1").src="./images/Motorcycles/M7.webp";
+}
+function h()
+{
+    document.getElementById("image1").src="./images/Motorcycles/M8.jpg";
+}
+function i()
+{
+    document.getElementById("image1").src="./images/Motorcycles/M9.jpg";
+}
+
+            // RIDES SECTION
+
+function r1()
+{
+    document.getElementById("ride1").src="./images/rides/rider mania22.webp";
+}
+function r2()
+{
+    document.getElementById("ride1").src="./images/rides/re mapp.webp";
+}
+
+        //  APPAREL SECTION
+
+function a1()
+{
+    document.getElementById("app1").src="./images/apparel/app1.webp";
+}
+function a2()
+{
+    document.getElementById("app1").src="./images/apparel/app2.0.webp";
+}
+function a3()
+{
+    document.getElementById("app1").src="./images/apparel/app3.0.jpg";
+}
+function a4()
+{
+    document.getElementById("app1").src="./images/apparel/app4.00.jfif";
+}
+function a5()
+{
+    document.getElementById("app1").src="./images/apparel/app5.0.webp";
+}
+function a6()
+{
+    document.getElementById("app1").src="./images/apparel/app6.jpg";
+}
+
+            // NAVBAR 5 Accesaries 
+
+function acc1()
+{
+    document.getElementById("ac1").src="./images/accessories/gma-home.webp";
+}
+function acc2()
+{
+    document.getElementById("ac1").src="./images/accessories/make-it-yors.jpg";
+}
+
+            //NAVBAR 6 (LOCATE US)
+
+function l1()
+{
+    document.getElementById("loc1").src="./images/Locate us/stores.jpg";
+}
+function l2()
+{
+    document.getElementById("loc1").src="./images/Locate us/service-centres.jpg";
+}
+function l3()
+{
+    document.getElementById("loc1").src="./images/Locate us/genuine-parts.jpg";
+}
